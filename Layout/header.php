3e44f5e7664ffe5453e13f02abf6033ba952201e
Raaaphhh <dcpraphael@gmail.com>
2026-03-12 09:57:52 +0100

    Fix

--- Layout/header.php
+++ Layout/header.php
@@ -51,7 +51,7 @@ if (isset($_POST['submit_logout'])) {
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="<?php echo BASE_URL; ?>">Groupy</a>
+        <a class="navbar-brand" href="<?= BASE_URL ?>/index.php">Groupy</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
