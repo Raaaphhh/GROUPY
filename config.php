44b58066b5bc5042437515ef0051489108a8fc5a
Raaaphhh <dcpraphael@gmail.com>
2026-03-12 11:42:36 +0100

    Fix / redirection from header

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('ROOT_PATH', __DIR__);
 $envBase = getenv('BASE_URL');
-define('BASE_URL', ($envBase !== false && $envBase !== '') ? $envBase : '/groupy');
+define('BASE_URL', $envBase !== false ? $envBase : '/groupy');
 require ROOT_PATH . '/Controlleur/UserController.php';
